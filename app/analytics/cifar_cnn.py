85b71b779378611e38c0fcee391ef4081fa075b0
Tola Awofolu <oawofolu@pivotal.io>
2023-02-09 15:29:09 -0500

    Updated README

--- app/analytics/cifar_cnn.py
+++ app/analytics/cifar_cnn.py
@@ -8,6 +8,10 @@ import tensorflow
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
+from sklearn.dummy import DummyClassifier
+import keras.backend as K
+from keras.layers import Input, Lambda
+from keras.models import Model
 import logging
 import pathlib
 import mlflow
@@ -24,53 +28,25 @@ import traceback
 from io import StringIO
 import http
 import requests
+import json
 from PIL import Image
 from mlflow.models import MetricThreshold
+from app.analytics import mlflow_utils
+from evidently.test_suite import TestSuite
+from evidently.test_preset import MulticlassClassificationTestPreset
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 warnings.filterwarnings('ignore')
 
 
-# ## Utilities
-def get_run_for_artifacts(active_run_id):
-    experiment_name = os.environ.get('MLFLOW_EXPERIMENT_NAME') or 'Default'
-    runs = mlflow.search_runs(experiment_names=[experiment_name], filter_string="tags.mainartifacts='y'", max_results=1,
-                              output_format='list')
-    if len(runs):
-        return runs[0].info.run_id
-    else:
-        mlflow.set_tags({'mainartifacts': 'y'})
-        return active_run_id
-
-
-def get_root_run(active_run_id=None):
-    experiment_name = os.environ.get('MLFLOW_EXPERIMENT_NAME') or 'Default'
-    runs = mlflow.search_runs(experiment_names=[experiment_name], filter_string="tags.runlevel='root'", max_results=1,
-                              output_format='list')
-    if len(runs):
-        parent_run_id = runs[0].info.run_id
-        mlflow.set_tags({'mlflow.parentRunId': parent_run_id})
-        return parent_run_id
-    else:
-        mlflow.set_tags({'runlevel': 'root'})
-        return active_run_id
-
-
-def get_current_run():
-    last_active_run = mlflow.last_active_run()
-    return last_active_run.info.run_id if last_active_run else None
-
-
 # ## Upload dataset
 
 # Upload dataset to S3 via MlFlow
 def upload_dataset(dataset, dataset_url=None):
-    experiment_id = mlflow.get_experiment_by_name(os.environ.get('MLFLOW_EXPERIMENT_NAME') or 'Default')
-
     with mlflow.start_run(run_name='upload_dataset', nested=True) as active_run:
-        mlflow.set_tags({'mlflow.parentRunId': get_root_run(active_run_id=active_run.info.run_id)})
-        artifact_run_id = get_run_for_artifacts(active_run.info.run_id)
+        mlflow_utils.prep_mlflow_run(active_run)
+        artifact_run_id = mlflow_utils.get_root_run(active_run.info.run_id)
         mlflow.environment_variables.MLFLOW_HTTP_REQUEST_TIMEOUT = '3600'
         os.environ['MLFLOW_HTTP_REQUEST_TIMEOUT'] = '3600'
 
@@ -102,7 +78,8 @@ def upload_dataset(dataset, dataset_url=None):
 def download_dataset(artifact):
     try:
         with mlflow.start_run(run_name='download_dataset', nested=True) as active_run:
-            artifact_run_id = get_run_for_artifacts(active_run.info.run_id)
+            mlflow_utils.prep_mlflow_run(active_run)
+            artifact_run_id = mlflow_utils.get_root_run(active_run.info.run_id)
             with mlflow.start_run(run_id=artifact_run_id, nested='True'):
                 uri = mlflow.get_artifact_uri(artifact_path=artifact)
                 uri = uri.replace('mlflow-artifacts:', f'{os.environ.get("MLFLOW_S3_ENDPOINT_URL")}/mlflow/artifacts')
@@ -112,7 +89,7 @@ def download_dataset(artifact):
                 os.makedirs(os.path.dirname(f'downloads/{artifact}'), exist_ok=True)
                 with open(download_path, 'wb') as f:
                     f.write(req.content)
-                    logging.info(f'{artifact} download complete.')
+                    logging.info(f'{artifact} download complete: {download_path}')
                 return download_path
     except http.client.IncompleteRead as icread:
         logging.info(f'Incomplete read...{icread}')
@@ -122,7 +99,9 @@ def download_dataset(artifact):
 
 
 def download_model(model_name, model_flavor, best_run_id=None, retries=2):
+    model, version = None, 0
     with mlflow.start_run(run_name='download_model', nested=True) as active_run:
+        mlflow_utils.prep_mlflow_run(active_run)
         try:
             client = MlflowClient()
             if best_run_id:
@@ -133,24 +112,24 @@ def download_model(model_name, model_flavor, best_run_id=None, retries=2):
             if len(versions) and versions[0].current_stage.lower() != 'production':
                 version = versions[0].version
                 model = getattr(mlflow, model_flavor).load_model(f'models:/{model_name}/{version}')
-                return model, version
             else:
                 logging.info(f"No suitable candidate model found for {model_name}...")
-                return None
         except Exception as e:
             if retries > 0:
                 logging.error(f"Could not download {model_name} - retrying...")
-                time.sleep(1)
+                time.sleep(10)
                 download_model(model_name, model_flavor, best_run_id=best_run_id, retries=retries - 1)
             else:
                 logging.error(f'Could not complete download for model {model_name} - error occurred: ', exc_info=True)
-                traceback.print_exc()
+        return model, version
 
 
 # ## Train Model
 def train_model(model_name, model_flavor, model_stage, data, epochs=10):
     with mlflow.start_run(run_name='train_model', nested=True) as active_run:
-        mlflow.set_tags({'mlflow.parentRunId': get_root_run(active_run_id=active_run.info.run_id)})
+        mlflow_utils.prep_mlflow_run(active_run)
+        artifact_run_id = mlflow_utils.get_root_run(active_run.info.run_id)
+        client = MlflowClient()
         # Build and Compile Model
         with tensorflow.device('/CPU:0'):  # Place tensors on the CPU
             model = models.Sequential()
@@ -178,12 +157,12 @@ def train_model(model_name, model_flavor, model_stage, data, epochs=10):
         plt.ylabel('Accuracy')
         plt.ylim([0.5, 1])
         plt.savefig("cifar_cnn_accuracy.png", bbox_inches='tight')
-        mlflow.log_artifact("cifar_cnn_accuracy.png")
+        client.log_artifact(artifact_run_id, "cifar_cnn_accuracy.png")
 
         # Log Metrics
         test_loss, test_acc = model.evaluate(data.get('test_data'), data.get('test_labels'), verbose=2)
-        mlflow.log_metric('testing_loss', test_loss)
-        mlflow.log_metric('testing_accuracy', test_acc)
+        client.log_metric(artifact_run_id, 'testing_loss', test_loss)
+        client.log_metric(artifact_run_id, 'testing_accuracy', test_acc)
         getattr(mlflow, model_flavor).autolog(log_models=False)
 
         # Register model
@@ -199,7 +178,7 @@ def evaluate_model(model_name, model_flavor):
     experiment_name = os.environ.get('MLFLOW_EXPERIMENT_NAME') or 'Default'
 
     with mlflow.start_run(run_name='evaluate_model', nested=True) as active_run:
-        mlflow.set_tags({'mlflow.parentRunId': get_root_run(active_run_id=active_run.info.run_id)})
+        mlflow_utils.prep_mlflow_run(active_run)
         best_runs = mlflow.search_runs(experiment_names=[experiment_name],
                                        filter_string="attributes.run_name='train_model'",
                                        order_by=['metrics.testing_accuracy DESC'],
@@ -217,79 +196,69 @@ def evaluate_model(model_name, model_flavor):
             )
 
 
-def promote_model_to_staging(base_model_name, candidate_model_name, model_flavor, evaluation_dataset_name):
+# ## Promote Model to Staging
+def promote_model_to_staging(base_model_name, candidate_model_name, evaluation_dataset_name, model_flavor):
     """
     Evaluates the performance of the currently trained candidate model compared to the base model.
     The model that performs better based on specific metrics is then promoted to Staging.
     """
 
     with mlflow.start_run(run_name='promote_model_to_staging', nested=True) as active_run:
-        mlflow.set_tags({'mlflow.parentRunId': get_root_run(active_run_id=active_run.info.run_id)})
+        mlflow_utils.prep_mlflow_run(active_run)
+        artifact_run_id = mlflow_utils.get_root_run(active_run.info.run_id)
+        client = MlflowClient()
 
-        _data = download_dataset(evaluation_dataset_name)
-        (candidate_model, candidate_model_version) = download_model(candidate_model_name, model_flavor)
-        (base_model, base_model_version) = download_model(base_model_name, model_flavor)
+        _data_path = download_dataset(evaluation_dataset_name)
+        _data = hkl.load(_data_path)
+        (candidate_model, candidate_model_version) = download_model(candidate_model_name, model_flavor, retries=6)
+        (base_model, base_model_version) = download_model(base_model_name, model_flavor, retries=6)
+        test_data = _data.get('test_data')
+        test_labels = _data.get('test_labels')
+        preexisting_base_model_found = base_model is not None
 
-        if base_model is None:
-            logging.info(
-                f"No prior base model found...setting up base model: name {base_model_name}, version {base_model_version}")
-            getattr(mlflow, model_flavor).log_model(base_model,
+        if not preexisting_base_model_found:
+            logging.info(f"No prior base model found with name {base_model_name}; preparing dummy model...")
+            size, num_classes = test_labels.shape[0], 10
+            dummy_data = pd.DataFrame({'x': np.random.randint(0, num_classes, size),
+                                       'y': test_labels.reshape(size, )})
+            base_model = DummyClassifier().fit(dummy_data['x'], dummy_data['y'])
+
+        # Generate and Save Evaluation Metrics
+        curr_data = _tensors_to_1d_prediction_and_target(test_data, test_labels, candidate_model)
+        ref_data = _tensors_to_1d_prediction_and_target(test_data, test_labels, base_model)
+        tests = TestSuite(tests=[
+            MulticlassClassificationTestPreset()
+        ])
+        tests.run(current_data=curr_data, reference_data=ref_data)
+
+        # Log Evaluation Metrics
+        tests_results_json = tests.json()
+        logging.info(f"Evidently generated results...{tests_results_json}")
+
+        # Save Evaluation Metrics Report
+        tests.save_html('/tmp/test_results.html')
+        client.log_artifact(artifact_run_id, "/tmp/test_results.html")
+        logging.info("Model evaluation report generated successfully.")
+
+        # Determine the best model
+        tests_results_json_tests = json.loads(tests_results_json)['tests']
+        accuracy_results = np.array([test for test in tests_results_json_tests if test['name'] == 'Accuracy Score'])
+        if len(accuracy_results):
+            client.log_metric(artifact_run_id, 'accuracy_score', accuracy_results[0]['parameters']['accuracy'])
+            promote_candidate_model = accuracy_results[0]['status'] == 'SUCCESS' or not preexisting_base_model_found
+
+            # Promote the best model
+            getattr(mlflow, model_flavor).log_model(candidate_model if promote_candidate_model else base_model,
                                                     artifact_path=base_model_name,
                                                     registered_model_name=base_model_name)
 
-        thresholds = {
-            "accuracy_score": MetricThreshold(
-                threshold=0.5,
-                min_absolute_change=0.01,
-                min_relative_change=0.01,
-                higher_is_better=True
-            ),
-        }
-
-        try:
-            mlflow.evaluate(
-                mlflow.models.get_model_info(f"models:/{candidate_model_name}/{candidate_model_version}"),
-                _data.get('test_data'),
-                targets=_data.get('test_labels'),
-                model_type="classifier",
-                validation_thresholds=thresholds,
-                baseline_model=mlflow.models.get_model_info(f"models:/{base_model_name}/{base_model_version}"),
+            client.transition_model_version_stage(
+                name=base_model_name,
+                version=base_model_version + 1,
+                stage="Staging"
             )
-        except Exception as e:
-            logging.error(f'Candidate model will not be promoted (will retain base model); failed threshold criteria')
-            traceback.print_exc()
-
-        """if candidate_model is None:
-            best_runs = mlflow.search_runs(filter_string="attributes.run_name='train_model'",
-                                           order_by=['metrics.testing_accuracy DESC'],
-                                           max_results=1,
-                                           search_all_experiments=True,
-                                           output_format='list')
-
-            logging.info(f"Best run found for promotion to staging is...{best_runs}")
-
-            best_run_id = best_runs[0].info.run_id if len(best_runs) else None
-
-            if best_run_id is not None:
-                mv = MlflowClient().search_model_versions(f'name like "{base_model_name}%" and run_id="{best_run_id}"')
-                if len(mv):
-                    registered_model_name = mv[0].name
-                    logging.info(
-                        f"Registered model name = {registered_model_name}, model being promoted = {base_model_name}")
-                    (model, version) = download_model(registered_model_name, model_flavor, best_run_id=best_run_id)
-                    getattr(mlflow, model_flavor).log_model(model,
-                                                            artifact_path=base_model_name,
-                                                            registered_model_name=base_model_name)
-
-                    logging.info(f"Found best model for model name {base_model_name}")
-
-                    MlflowClient().transition_model_version_stage(
-                        name=base_model_name,
-                        version=version,
-                        stage="Staging"
-                    )
-                else:
-                    logging.error("Could not promote a model to Staging (no models found to promote).")"""
+        else:
+            raise Exception("ERROR: Failed to identify model for promotion.")
 
 
 # ## Make Prediction
@@ -314,3 +283,11 @@ def predict(img, model_name, model_stage):
     logging.info(
         f'Predictions in order...list={prediction_results}, index={np.argmax(prediction_results)}, prediction={prediction}')
     return prediction
+
+
+def _tensors_to_1d_prediction_and_target(tensor_data, tensor_labels, tensor_model):
+    predictions = tensor_model.predict(tensor_data)
+    predictions = predictions.reshape(predictions.shape[0], -1)
+    data = pd.DataFrame({'prediction': np.fromiter((np.argmax(x) for x in predictions), dtype='int'),
+                         'target': tensor_labels.reshape(tensor_labels.shape[0], )})
+    return data
