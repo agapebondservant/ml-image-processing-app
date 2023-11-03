705e0b251c9813343af53b6b5104fd2ed449e5ea
Tola Awofolu <oawofolu@pivotal.io>
2023-11-03 11:45:08 -0400

    Testing

--- config/shell/pre-commit-hook.sh
+++ config/shell/pre-commit-hook.sh
@@ -1,4 +1,4 @@
 echo "Generate kapp-controller marker............."
-echo "\"marker\": \"$(git rev-parse --short HEAD)\"" > kapp-marker
+echo "marker: \"$(git rev-parse --short HEAD)\"" > kapp-marker
 echo "Adding kapp-controller marker to repo............."
 git add kapp-marker
