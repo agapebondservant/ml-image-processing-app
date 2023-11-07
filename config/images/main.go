fbf2ac981931073fed5bffbad483e277afc8c505
Tola Awofolu <oawofolu@pivotal.io>
2023-11-07 02:37:46 -0500

    Testing

--- config/images/main.go
+++ config/images/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"log"
+    "os/exec"
 )
 
 func main() {
-    log.Println("Success")
+    cmd := exec.Command("/bin/sh", "-c",  "echo \"sha: \"$(date +\"%Y%m%d%H%M%S\") > /workspace/kapp-marker.yaml")
+    stdout, err := cmd.Output()
+    if err == nil {
+        log.Println(string(stdout))
+    } else {
+        log.Fatal(err)
+    }
 }
